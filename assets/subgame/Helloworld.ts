0e51a9af2c56926bdf382df2f146427b88dd7850
chad <Chad.hsiao@goldenf.co>
2021-05-04 15:56:45 +0800

    [feat]

--- assets/subgame/Helloworld.ts
+++ assets/subgame/Helloworld.ts
@@ -1,3 +1,6 @@
+import { divide } from "lodash";
+import { GameRoot } from "./script/GameRoot";
+
 const {ccclass, property} = cc._decorator;
 
 @ccclass
@@ -9,17 +12,43 @@ export default class Helloworld extends cc.Component {
     @property
     text: string = 'hello';
 
+    private ab_bundle:cc.AssetManager.Bundle=null;
+
     start () {
-        // init logic
-        //this.label.string = this.text;
 
-        console.log("chad release cc_101");
-        cc.resources.release("cc_101");
+        console.log("lodash2 ",divide(10,5));
+        //init abgame
+        let self = this;
+        if( self.ab_bundle === null ){
+            cc.assetManager.loadBundle('http://localhost:8081/assets/abtest', function (err, bundle) {
+                if (err) {
+                    return console.error(err);
+                }
+                console.log('load ab bundle successfully.',bundle);
+                self.ab_bundle = bundle;
+                bundle.load('testmgame', cc.Prefab, function (err, prefab) {
+                    console.log("prefab load",err,prefab)
+                    let pf:cc.Prefab = prefab as cc.Prefab;
+                    let newNode = cc.instantiate(pf);
+                    //newNode.x = cc.director.getScene().width/2;
+                    //newNode.y = cc.director.getScene().height/2;
+                    let child = cc.director.getScene().getChildByName('Canvas');
+                    child.addChild(newNode);
+                });
+            });
+        }
 
+        //console.log("chad release cc_101");
+        //cc.resources.release("cc_101");
+        cc.game.emit('fire',new Date());
         this.scheduleOnce(function() {
             cc.find("LobbyNode").getComponent("LobbyNode").debug();
             console.log("chad2");
             cc.director.loadScene("game_cc_101");
+            
         }, 2);
+
+        let test:GameRoot = new GameRoot();
+        test.debug();
     }
 }
---
+++ assets/subgame/script/GameRoot.ts
@@ -0,0 +1,6 @@
+export class GameRoot {
+    public data:number=9;
+    public debug():void{
+        console.log("sub game3 debug");
+    }
+}
